6b5c0e52e541ea8fbbe2d4023c6346e1392f4fde
hollowillow <slavicekmichal@proton.me>
2025-12-29 00:19:55 +0100

    Configure filesystem mounting

--- configuration.nix
+++ configuration.nix
@@ -1,101 +1,72 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
+  imports = [
       ./hardware-configuration.nix
     ];
 
-  boot.loader.grub = {
-  	enable = true;
-	device = "/dev/vda";
-	useOSProber = true;
-  };
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = false;
 
-  networking = {
-  	hostName = "nixos";
-	networkmanager.enable = true;
-  	# wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-	# proxy = { # Configure network proxy if necessary
-	# 	default = "http://user:password@proxy:port/";
-	#  	noProxy = "127.0.0.1,localhost,internal.domain";
-	# };
-  };
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Set your time zone.
-  time.timeZone = "Europe/Prague";
-
-  # Select internationalisation properties.
+  time.timeZone = "Europe/Amsterdam";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable sound with pipewire.
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable hyprland
+  programs.hyprland.enable = true;
+  # programs.hyprland.xwayland.enable = true;
+  programs.waybar.enable = true;
+
+  services.getty = {
+  	autologinUser = "hollowillow";
+	autologinOnce = true;
+  };
+  environment.loginShellInit = ''
+  	[[ "$(tty)" == /dev/tty1 ]] && hyprland
+  '';
+
+  # Enable sound.
   services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.groups.data = {};
   users.users.hollowillow = {
     isNormalUser = true;
-    description = "hollowillow";
-    extraGroups = [ "networkmanager" "wheel" ];
+    initialPassword = "asdf";
+    extraGroups = [ "wheel" "data" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
-    	firefox # browser
-	stow
-	# keepassxc # password database
-	# pfetch
-	# btop
-	# mpv
-	# steam # can be enabled as program
-	# retroarch
+      firefox
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  security.sudo.enable = true;
 
-  # system wide packages
-  # $ nix search (package)
+  # programs.firefox.enable = true;
+
   environment.systemPackages = with pkgs; [
-     neovim
-     exfatprogs # exfat utils
-     noto-fonts # character compatibility
-     git # version control # can be enabled as program
-  #  wget
+    neovim
+    git
+    wget
+    kitty
+    alacritty
+    wl-clipboard
   ];
-
-   # security = {
-   # 	sudo.enable = false;
-   # 	doas.enable = true;
-   # };
-
-  # Enable Sway
-  programs.sway.enable = true;
-
-  # Enable autologin
-  services.getty = {
-  	autologinUser = "hollowillow";
-	# autologinOnce = true;
-  };
-
-  # Autostart Sway
-  environment.loginShellInit = ''
-  	[[ "$(tty)" == /dev/tty1 ]] && sway
-  '';
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -106,7 +77,11 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  system.stateVersion = "25.11";
 }
+
